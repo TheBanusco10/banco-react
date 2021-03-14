65d99ce5d3a273a72e56f370c512cf4d9c5b72d6
thebanusco10 <thebanusco10@gmail.com>
2021-03-14 08:43:46 +0100

    App

--- src/components/Cuenta.js
+++ src/components/Cuenta.js
@@ -9,7 +9,7 @@ export const Cuenta = ({ibanCuenta, numeroCuenta, saldoCuenta, interesAnualCuent
             <div className="contenedor">
                 <p><strong>IBAN:</strong> {ibanCuenta}</p>
                 <p><strong>Número de cuenta:</strong> {numeroCuenta}</p>
-                <p><strong>Saldo en la cuenta:</strong> {saldoCuenta}</p>
+                <p><strong>Saldo en la cuenta:</strong> {saldoCuenta}€</p>
                 <p><strong>Interés anual:</strong> {interesAnualCuenta}</p>
                 <p><strong>Tipo de cuenta:</strong> {tipoCuenta}</p>
             </div>
--- src/hooks/useFetch.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const useFetch = () => {
-    
-    const [data, setData] = useState([]);
-
-    
-
-}
