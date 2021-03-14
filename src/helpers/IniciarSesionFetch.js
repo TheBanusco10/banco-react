5e0eefc82fdceb07fd8e53cf0024f781ebfd81e0
thebanusco10 <thebanusco10@gmail.com>
2021-03-14 09:05:07 +0100

    Base app

--- src/helpers/IniciarSesionFetch.js
+++ src/helpers/IniciarSesionFetch.js
@@ -1,10 +1,7 @@
-import React from 'react'
-
 export const IniciarSesionFetch = async (dni) => {
 
-    console.log(dni);
     
-    let res = await fetch('/src/php/iniciarSesion.php', {
+    let res = await fetch('http://localhost:8000/src/php/iniciarSesion.php', {
         method: 'POST',
         mode: 'cors', // no-cors, *cors, same-origin
         headers: new Headers({
