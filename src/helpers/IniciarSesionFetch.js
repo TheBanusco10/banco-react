253318e5e5617cac48cdc46b90a40b5e378ba2a9
thebanusco10 <thebanusco10@gmail.com>
2021-03-14 09:10:20 +0100

    Base app

--- src/helpers/IniciarSesionFetch.js
+++ src/helpers/IniciarSesionFetch.js
@@ -1,7 +1,7 @@
 export const IniciarSesionFetch = async (dni) => {
 
     
-    let res = await fetch('http://localhost:8000/src/php/iniciarSesion.php', {
+    let res = await fetch('https://banco-react.herokuapp.com/src/php/iniciarSesion.php', {
         method: 'POST',
         mode: 'cors', // no-cors, *cors, same-origin
         headers: new Headers({
